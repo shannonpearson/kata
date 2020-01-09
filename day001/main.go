dab08ff073e271c1f1527cf6344135e8593139da
shannonpearson <shannon.pearson@goverb.com>
2020-01-09 14:45:06 -0600

    initial commit: day 1

---
+++ day001/main.go
@@ -0,0 +1,21 @@
+package main
+
+// TwoSum takes an array of integers and a target numbers and returns the
+// indices of the integers whose sum is equal to the target number.
+func TwoSum(numbers []int, target int) [2]int {
+	indices := [2]int{}
+outer:
+	for i, num := range numbers {
+		for j, n := range numbers[i+1:] {
+			if num+n == target {
+				indices = [2]int{i, i + j + 1}
+				break outer
+			}
+		}
+	}
+	return indices
+}
+func main() {
+	numbers := []int{4, 5, 2, 9}
+	TwoSum(numbers, 13)
+}
