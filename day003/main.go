e875d820f7ea79b0db8792b3f719fc16fb32ab04
shannonpearson <shannon.pearson@goverb.com>
2020-01-13 12:42:23 -0600

    day 3

---
+++ day003/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// DigPow does a thing
+func DigPow(n, p int) int {
+	num := strconv.Itoa(n)
+	digits := strings.Split(num, "")
+
+	sum := 0
+	for i, d := range digits {
+		d, err := strconv.Atoi(d)
+		if err != nil {
+			fmt.Println(err)
+			return 0
+		}
+		sum += int(math.Pow(float64(d), float64(i+p)))
+	}
+	result := sum % n
+	if result == 0 {
+		return sum / n
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(DigPow(46288, 3))
+}
