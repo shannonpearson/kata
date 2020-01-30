d220d836dd911db5f962c1a55409fd22a070a8c3
shannonpearson <shannon.pearson@goverb.com>
2020-01-30 15:15:54 -0600

    removed commented contents of entry point

--- main/main.go
+++ main/main.go
@@ -1,30 +1,5 @@
 package main
 
 func main() {
-	// // Day 1
-	// fmt.Println(kata.TwoSum([]int{4, 5, 2, 9}, 13))
-	// // Day 2
-	// fmt.Println(kata.CountBits(102))
-	// // Day 3
-	// fmt.Println(kata.DigPow(46288, 3))
-	// // Day 4
-	// s := 15
-	// x := []float64{0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25}
-	// fmt.Println(kata.Gps(s, x))
-	// // Day 5
-	// oddsInput := []int{
-	// 	20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5,
-	// }
-	// fmt.Println(kata.FindOdd(oddsInput))
-	// // Day 6
-	// fmt.Println(kata.BouncingBall(3.0, 0.66, 1.5))
-	// // Day 7
-	// nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
-	// fmt.Println(kata.MaximumSubarraySum(nums))
-	// // Day 8
-	// peaksInput := []int{3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3}
-	// fmt.Println(kata.PickPeaks(peaksInput))
-	// Day 9
-	// fmt.Println(backwardsPrime(2, 100))
-	// Day 10
+
 }
