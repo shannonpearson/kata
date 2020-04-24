aed8a1af7c19d6017befe84c86a8e1dcc71de97b
Shannon Pearson <schristinep@gmail.com>
2020-04-23 22:10:03 -0400

    day 12 handles negatives and duplicates

--- kata/day012.go
+++ kata/day012.go
@@ -9,6 +9,7 @@ import (
 // there is probably a faster way to find all the factors...
 func getPrimeFactors(i int) []int {
 	c := i
+	i = int(math.Abs(float64(i)))
 
 	factors := []int{}
 	for j := 2; j <= int(math.Sqrt(float64(i))); j++ {
@@ -22,31 +23,43 @@ func getPrimeFactors(i int) []int {
 	if i == c {
 		factors = append(factors, i)
 	}
-	return factors
+
+	uniqDict := make(map[int]bool)
+	for _, n := range factors {
+		uniqDict[n] = true
+	}
+
+	uniq := []int{}
+	for k := range uniqDict {
+		uniq = append(uniq, k)
+	}
+
+	return uniq
 }
 
 func SumOfDivided(lst []int) string {
 	result := ""
 	primes := []int{}
-	dict := make(map[int]map[int]bool)
+	dict := make(map[int][]int)
 
 	for _, n := range lst {
 		factors := getPrimeFactors(n)
 		for _, f := range factors {
 			if dict[f] == nil {
-				dict[f] = make(map[int]bool)
+				dict[f] = []int{}
 				primes = append(primes, f)
 			}
-			dict[f][n] = true
+			dict[f] = append(dict[f], n)
 		}
 
 	}
+	fmt.Println(dict)
 
 	sort.Ints(primes)
 	for _, value := range primes {
 		sum := 0
-		for k := range dict[value] {
-			sum += k
+		for _, m := range dict[value] {
+			sum += m
 		}
 		result += fmt.Sprintf("(%d %d)", value, sum)
 	}
